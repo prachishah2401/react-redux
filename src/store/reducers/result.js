5c3f8fa846275f4ca2a12f5fbef2e3fa8a002c6e
Prachi <prachishah2401@gmail.com>
2020-08-27 19:01:31 +1000

    restructuring actions and using action creators and getState. Using utility function

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -1,26 +1,23 @@
-import * as actionTypes from '../actions/actions';
+import * as actionTypes from '../actions/actionsTypes';
+import { updateObject } from '../utility';
+import { storeResult } from '../actions';
 
 const initialState ={
     results: []
 }
 
+const deleteResult = (state, action) => {
+    const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+    return updateObject(state, {results: updatedArray});
+}
 const resultreducer = (state = initialState, action) => {
 
     switch (action.type) {
         case actionTypes.STORE_RESULT:
-            return {
-                ...state,
-                results: state.results.concat({id: new Date(), value: action.result})
-            }
+            return updateObject(state, {results: state.results.concat({id: new Date(), value: action.result * 2})});
+
         case actionTypes.DELETE_RESULT:
-            // const id = 2;
-            // const newArray = [...state.results]
-            // newArray.splice(id, 1)
-            const updatedArray = state.results.filter(result => result.id !== action.resultElId);
-            return {
-                ...state,
-                results: updatedArray
-            }
+            return deleteResult(state, action);
     }
     return state;
 };
